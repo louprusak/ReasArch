7672245c5c709fe919ef1ca54e54bb9c8eeafc3f
lorusak <lorusak@3994cbb5-1302-4bbd-8812-0d1d036f0faf>
2020-05-03 13:50:11 +0000

    Ajout du projet test avec des users controls dans le dossier branches

---
+++ branches/ReasArch/ReasArch/BoutonListeBatiments.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReasArch
+{
+    /// <summary>
+    /// Logique d'interaction pour BoutonListeBatiments.xaml
+    /// </summary>
+    public partial class BoutonListeBatiments : UserControl
+    {
+        public BoutonListeBatiments()
+        {
+            InitializeComponent();
+        }
+
+        public System.Windows.Media.ImageSource Source
+        {
+            set
+            {
+                Image.Source = value;
+            }
+        }
+
+        public string NomBat
+        {
+            set
+            {
+                Titre.Text = value;
+            }
+        }
+    }
+}
