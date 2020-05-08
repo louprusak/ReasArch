c78bc6071ae3307efcda667fd28dd253e74bc04b
lorusak <lorusak@3994cbb5-1302-4bbd-8812-0d1d036f0faf>
2020-05-08 19:14:01 +0000

    Remplacement du projet principal dans le dossier trunk avec celui se trouvant dans le dossier branches

---
+++ trunk/source/ReasArch/ReasArch/BoutonListeVilles.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReasArch
+{
+    /// <summary>
+    /// Logique d'interaction pour BoutonListeVilles.xaml
+    /// </summary>
+    public partial class BoutonListeVilles : UserControl
+    {
+        public BoutonListeVilles()
+        {
+            InitializeComponent();
+        }
+
+        public System.Windows.Media.ImageSource Source
+        {
+            set
+            {
+                IMGVille.Source = value;
+            }
+        }
+
+        public string Texte
+        {
+            set
+            {
+                TBListeVille.Text = value;
+            }
+        }
+
+        
+
+    }
+}
