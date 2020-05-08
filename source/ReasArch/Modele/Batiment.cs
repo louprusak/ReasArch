9b12b454e5e0f47281ff2b9e8189d200def7378f
lorusak <lorusak@3994cbb5-1302-4bbd-8812-0d1d036f0faf>
2020-05-08 20:28:17 +0000

    Début de la rédaction des classes en csharp : - ajout du projet modele avec :     . classe Batiment     . classe Ville     . classe Manager - ajout du projet Test

---
+++ source/ReasArch/Modele/Batiment.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modele
+{
+    public class Batiment
+    {
+        /// <summary>
+        /// Nom du batiment
+        /// </summary>
+        public string Nom { get; private set; }
+
+        /// <summary>
+        /// Ville dans laquelle se trouve le batiment
+        /// </summary>
+        public string Ville { get; private set; }
+
+        /// <summary>
+        /// Nom de l'architecte du batiment
+        /// </summary>
+        public string Architecte { get; private set; }
+
+        /// <summary>
+        /// Description autre du batiment
+        /// </summary>
+        public string Description { get; private set; }
+
+        public Batiment (string nom, string architecte, string description)
+        {
+            Nom = nom;
+            Architecte = architecte;
+            Description = description;
+        }
+
+        public Batiment (string nom, string ville, string architecte, string description)
+            : this(nom, architecte, description)
+        {
+            Ville = ville;
+        }
+    }
+}
---
+++ source/ReasArch/Modele/Manager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modele
+{
+    class Manager
+    {
+        public List<Ville> Listevilles { get; private set; }
+
+        public Manager()
+        {
+            List<Ville> liste = new List<Ville>();
+            Listevilles = liste;
+        }
+
+        public bool AjouterVille (string nom)
+        {
+            Ville ville = new Ville(nom);
+            Listevilles.Add(ville);
+            return true;
+        }
+
+        public bool SupprimerVille (string nom)
+        {
+            return true;
+        }
+
+        public bool ModifierVille (string nom)
+        {
+            return true;
+        }
+
+        public bool AjouterBatiment(string nomville, string nombat, string architecte, string descriptionbat)
+        {
+            ///recherche de la ville avec le nom dans la liste
+            ///appel de la fonction ajouter batiment depuis l'élément de la liste ville
+            return true;
+        }
+
+        public bool SupprimerBatiment (string nomville, string nombat)
+        {
+            return true;
+        }
+
+        public bool ModifierBatiment( string nomville, string nombat)
+        {
+            return true;
+        }
+    }
+}
