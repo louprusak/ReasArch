77edf43fd465ca415572dc28cda12ea987fda04f
lorusak <lorusak@3994cbb5-1302-4bbd-8812-0d1d036f0faf>
2020-05-08 20:28:17 +0000

    Début de la rédaction des classes en csharp : - ajout du projet modele avec :     . classe Batiment     . classe Ville     . classe Manager - ajout du projet Test

---
+++ trunk/source/ReasArch/Modele/Ville.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modele
+{
+    public class Ville
+    {
+        public string Nom { get; private set; }
+
+        public List<Batiment> Listebatiments { get; private set; }
+
+        public Ville (string nom)
+        {
+            Nom = nom;
+            Listebatiments = new List<Batiment>();
+        }
+        
+        public bool AjouterBatiment ( string nom, string architecte, string description)
+        {
+            Batiment batiment = new Batiment(nom, architecte, description);
+            Listebatiments.Add(batiment);
+            return true;
+        }
+
+
+    }
+}
