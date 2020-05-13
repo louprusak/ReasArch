346a7d1961f4c8c4d1804f2f8e372c738c59804c
lorusak <lorusak@3994cbb5-1302-4bbd-8812-0d1d036f0faf>
2020-05-13 08:07:44 +0000

    Modifications de la classe Batiment ainsi que de son diagramme UML

--- source/ReasArch/Modele/Batiment.cs
+++ source/ReasArch/Modele/Batiment.cs
@@ -1,61 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Text;
 
 namespace Modele
 {
     public class Batiment
     {
-        /// <summary>
-        /// Nom du batiment
-        /// </summary>
-        public string Nom { get; private set; }
+        public string Nom { get => Nom.ToUpper(); private set { } } //Nom du batiment
+        public string Pays { get; private set; }//Pays dans lequel se trouve le batiment
+        public string Ville { get; private set; }//Ville dans laquelle se trouve le batiment
+        public string Quartier { get; private set; } = "non spécifié";//Quartier dans lequel se trouve la batiment
+        public string Adresse { get; private set; } = "non spécifié";//Adresse du batiment
+        public string Architecte { get; private set; } = "non spécifié";//Architecte du batiment
+        public string Ingenieur { get; private set; } = "non spécifié";//INgénieur du batiment
+        public string Style { get; private set; } = "non spécifié";//Style du batiment
+        public string Materiaux { get; private set; } = "non spécifié";//Matériaux utilisés pour construire le batiment
+        public string Hauteur { get; private set; } = "non spécifié";//Hauteur en mètres du batiment
+        public string NbEtages { get; private set; } = "non spécifié";//Nombre d'étages du batiment
+        public string Construction { get; private set; } = "non spécifié";//Années de construction du batiment
+        public string Ouverture { get; private set; } = "non spécifié";//Année d'ouverture du batiment
+        public string Description { get; private set; }//Description autre du batiment
 
-        /// <summary>
-        /// Ville dans laquelle se trouve le batiment
-        /// </summary>
-        public string Ville { get; private set; }
+        //----------------------------------------------------------------------------------------------------------------------//
+        //CONSTRUCTEURS
 
-        /// <summary>
-        /// Nom de l'architecte du batiment
-        /// </summary>
-        public string Architecte { get; private set; }
-
-        /// <summary>
-        /// Description autre du batiment
-        /// </summary>
-        public string Description { get; private set; }
-
-        /// <summary>
-        /// Constructeur de Batiment
-        /// </summary>
-        /// <param name="nom">Nom du bâtiment</param>
-        /// <param name="architecte">Nom de l'architecte du batiment</param>
-        /// <param name="description">Description du batiment</param>
-        public Batiment (string nom, string architecte, string description)
+        public Batiment(string nom, string pays, string ville, string description)
         {
-            Nom = nom;
+            if (String.IsNullOrEmpty(nom) | String.IsNullOrEmpty(pays) | String.IsNullOrEmpty(ville) | String.IsNullOrEmpty(description))
+            {
+                Nom = nom;
+                Pays = pays;
+                Ville = ville;
+                Description = description;
+            }
+        }
+
+
+        public Batiment(string nom, string pays, string ville, string quartier, string adresse, string architecte,
+            string ingenieur, string style, string materiaux, string hauteur, string nbetages, string construction,
+            string ouverture, string description)
+            : this(nom, pays, ville, description)
+        {
+            Quartier = quartier;
+            Adresse = adresse;
             Architecte = architecte;
-            Description = description;
+            Ingenieur = ingenieur;
+            Style = style;
+            Materiaux = materiaux;
+            Hauteur = hauteur;
+            NbEtages = nbetages;
+            Construction = construction;
+            Ouverture = ouverture;
         }
 
-        /// <summary>
-        /// Constructeur de Batiment
-        /// </summary>
-        /// <param name="nom">Nom du batiment</param>
-        /// <param name="ville">Nom de la ville dans laquelle se trouve le batiment</param>
-        /// <param name="architecte">Nom de l'architecte du batiment</param>
-        /// <param name="description">Description du batiment</param>
-        public Batiment (string nom, string ville, string architecte, string description)
-            : this(nom, architecte, description)
+        //----------------------------------------------------------------------------------------------------------------------//
+        //METHODES  
+
+        public override string ToString()
         {
-            Ville = ville;
+            string message = $"Nom : {Nom}\nPays : {Pays}\nQuartier : {Quartier}\nAdresse : {Adresse}\nArchitecte : {Architecte}\n" +
+                $"Ingénieur : {Ingenieur}\nStyle : {Style}\nMatériaux : {Materiaux}\nHauteur : {Hauteur}\nNombre d'Etages : {NbEtages}\n" +
+                $"Construction : {Construction}\nOuverture : {Ouverture}\n\n" +
+                $"{Description}";
+            return message;
         }
-
-        /// <summary>
-        /// Méthode ToString d'un batiment qui permet de résumer les infos essentielles sur une instance de batiment
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString() => $"Nom: {Nom}\nArchitecte: {Architecte}\nDescription: {Description}";
     }
 }
+
