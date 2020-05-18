58530934069e1244f7d5a1a4483a02a467aaf899
lorusak <lorusak@3994cbb5-1302-4bbd-8812-0d1d036f0faf>
2020-05-18 14:20:37 +0000

    Modifications sur les classes csharp

--- source/ReasArch/Modele/Batiment.cs
+++ source/ReasArch/Modele/Batiment.cs
@@ -7,20 +7,20 @@ namespace Modele
 {
     public class Batiment
     {
-        public string Nom { get => Nom.ToUpper(); private set { } } //Nom du batiment
-        public string Pays { get; private set; }//Pays dans lequel se trouve le batiment
-        public string Ville { get; private set; }//Ville dans laquelle se trouve le batiment
-        public string Quartier { get; private set; } = "non spécifié";//Quartier dans lequel se trouve la batiment
-        public string Adresse { get; private set; } = "non spécifié";//Adresse du batiment
-        public string Architecte { get; private set; } = "non spécifié";//Architecte du batiment
-        public string Ingenieur { get; private set; } = "non spécifié";//INgénieur du batiment
-        public string Style { get; private set; } = "non spécifié";//Style du batiment
-        public string Materiaux { get; private set; } = "non spécifié";//Matériaux utilisés pour construire le batiment
-        public string Hauteur { get; private set; } = "non spécifié";//Hauteur en mètres du batiment
-        public string NbEtages { get; private set; } = "non spécifié";//Nombre d'étages du batiment
-        public string Construction { get; private set; } = "non spécifié";//Années de construction du batiment
-        public string Ouverture { get; private set; } = "non spécifié";//Année d'ouverture du batiment
-        public string Description { get; private set; }//Description autre du batiment
+        public string Nom { get => Nom.ToUpper(); set { } } //Nom du batiment
+        public string Pays { get; set; }//Pays dans lequel se trouve le batiment
+        public string Ville { get; set; }//Ville dans laquelle se trouve le batiment
+        public string Quartier { get; set; } = "non spécifié";//Quartier dans lequel se trouve la batiment
+        public string Adresse { get;  set; } = "non spécifié";//Adresse du batiment
+        public string Architecte { get; set; } = "non spécifié";//Architecte du batiment
+        public string Ingenieur { get; set; } = "non spécifié";//INgénieur du batiment
+        public string Style { get; set; } = "non spécifié";//Style du batiment
+        public string Materiaux { get; set; } = "non spécifié";//Matériaux utilisés pour construire le batiment
+        public string Hauteur { get; set; } = "non spécifié";//Hauteur en mètres du batiment
+        public string NbEtages { get; set; } = "non spécifié";//Nombre d'étages du batiment
+        public string Construction { get; set; } = "non spécifié";//Années de construction du batiment
+        public string Ouverture { get; set; } = "non spécifié";//Année d'ouverture du batiment
+        public string Description { get; set; }//Description autre du batiment
 
         //----------------------------------------------------------------------------------------------------------------------//
         //CONSTRUCTEURS
@@ -59,8 +59,23 @@ namespace Modele
             Ouverture = ouverture;
         }
 
+        public bool ModifierBatiment(string nvnom, string pays, string ville, string quartier,
+            string adresse, string architecte, string ingenieur, string style, string materiaux, string hauteur,
+            string nbetages, string construction, string ouverture, string description)
+        {
+            Nom = nvnom; Pays = pays; Ville = ville;  Quartier = quartier; Adresse = adresse;
+            Architecte = architecte; Ingenieur = ingenieur; Style = style; Materiaux = materiaux;
+            Hauteur = hauteur; NbEtages = nbetages; Construction = construction;
+            Ouverture = ouverture; Description = description;
+
+            if(Nom == nvnom && Pays == pays && Ville == ville && Quartier == quartier && Adresse == adresse &&
+            Architecte == architecte && Ingenieur == ingenieur && Style == style && Materiaux == materiaux &&
+            Hauteur == hauteur && NbEtages == nbetages && Construction == construction &&
+            Ouverture == ouverture && Description == description) return true;
+            else return false;
+
         //----------------------------------------------------------------------------------------------------------------------//
-        //METHODES  
+        //REDEFINITION DE METHODES 
 
         public override string ToString()
         {
