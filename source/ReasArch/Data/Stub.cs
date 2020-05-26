c57baf3278ad126519361a3ab4cccb258a08609c
lorusak <lorusak@3994cbb5-1302-4bbd-8812-0d1d036f0faf>
2020-05-26 15:50:19 +0000

    Ajout du fichier Word et modifications sur la classe Stub et Program

--- source/ReasArch/Data/Stub.cs
+++ source/ReasArch/Data/Stub.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,21 +11,60 @@ namespace Data
 {
     public class Stub
     {
-        public static Ville CreerVille()
+        public static Manager CreerManager()
         {
-            Ville ville1 = new Ville("Clermont");
-            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
-            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
-            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
-            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
-            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
-            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
-            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
-            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
-            return ville1;
+            Manager manager = new Manager();
+            manager.AjouterVille("Clermont-Ferrand");
+            manager.AjouterVille("Lyon");
+            manager.AjouterVille("Paris");
+
+            manager.AjouterBatimentSimple("Clermont-Ferrand", "Cathédrale", "France", "Clermont-Ferrand", "Description");
+            manager.AjouterBatimentSimple("Clermont-Ferrand", "Cathédrale", "France", "Clermont-Ferrand", "Description");
+            manager.AjouterBatimentSimple("Clermont-Ferrand", "Cathédrale", "France", "Clermont-Ferrand", "Description");
+            manager.AjouterBatimentSimple("Clermont-Ferrand", "Cathédrale", "France", "Clermont-Ferrand", "Description");
+            manager.AjouterBatimentSimple("Clermont-Ferrand", "Cathédrale", "France", "Clermont-Ferrand", "Description");
+
+            manager.AjouterBatimentSimple("Lyon", "Tour Crayon", "France", "Lyon", "Description");
+            manager.AjouterBatimentSimple("Lyon", "Tour Crayon", "France", "Lyon", "Description");
+            manager.AjouterBatimentSimple("Lyon", "Tour Crayon", "France", "Lyon", "Description");
+            manager.AjouterBatimentSimple("Lyon", "Tour Crayon", "France", "Lyon", "Description");
+            manager.AjouterBatimentSimple("Lyon", "Tour Crayon", "France", "Lyon", "Description");
+
+            manager.AjouterBatimentSimple("Paris", "Tour Effeil", "France", "Paris", "Description");
+            manager.AjouterBatimentSimple("Paris", "Tour Effeil", "France", "Paris", "Description");
+            manager.AjouterBatimentSimple("Paris", "Tour Effeil", "France", "Paris", "Description");
+            manager.AjouterBatimentSimple("Paris", "Tour Effeil", "France", "Paris", "Description");
+            manager.AjouterBatimentSimple("Paris", "Tour Effeil", "France", "Paris", "Description");
+
+            return manager;
         }
 
+        public static Ville CreerVille()
+        {
+            Ville ville = new Ville("Londres");
+            ville.AjouterBatimentSimple("BigBen", "Angleterre", "Londres", "Description");
+            ville.AjouterBatimentSimple("LondonEye", "Angleterre", "Londres", "Description");
 
+            return ville;
+        }
 
+        public static Batiment CreerBatimentAvecNom()
+        {
+            Batiment batiment = new Batiment("Centre Jaude");
+            return batiment;
+        }
+
+        public static Batiment CreerBatimentSimple()
+        {
+            Batiment batiment = new Batiment("Cathédrale", "France", "Clermont-Ferrand", "Ceci est la decription de la cathédrale de Clermont-Ferrand");
+            return batiment;
+        }
+
+        public static Batiment CreerBatimentComplet()
+        {
+            Batiment batiment = new Batiment("Tour de Pise","Italie","Pise","Pise","adresse pise", "on sait pas","pas qui c'est","vieux","pierre","haut","compte pour voir","ya longtemps",
+                "très longtemps","bah elle penche quoi ...");
+            return batiment;
+        }
     }
 }
--- source/ReasArch/Test/Program.cs
+++ source/ReasArch/Test/Program.cs
@@ -2,6 +2,8 @@
 using System.ComponentModel;
 using static System.Console;
 using Modele;
+using Data;
+
 
 
 namespace Test
@@ -15,39 +17,42 @@ namespace Test
                 "------------------------------------------------------------------------------------------------------------\n\n");
             WriteLine("1) Instanciation de la classe Batiment:\n\n");
 
-            Batiment batiment1 = new Batiment("Centre Jaude");
-            WriteLine("ToString du Batiment avec seulement le nom:\n"+batiment1.ToString());
+            
+            WriteLine("ToString du Batiment avec seulement le nom:\n"+Stub.CreerBatimentAvecNom().ToString());
             WriteLine("-    -   -   -   -   -");
-            Batiment batiment2 = new Batiment("Cathédrale", "France","Clermont-Ferrand", "Ceci est la decription de la cathédrale de Clermont-Ferrand");
-            WriteLine("ToString du Batiment avec détail simplifié:\n"+batiment2.ToString());
+            
+            WriteLine("ToString du Batiment avec détail simplifié:\n"+Stub.CreerBatimentSimple().ToString());
             WriteLine("-    -   -   -   -   -");
-            Batiment batiment3 = new Batiment("Centre Jaude 2", "France", "Clermont-Ferrand", "Centre de Clermont", "Place de Jaude", "M.Jenesaispas", "M.Jenesaisqui",
-                "Comtemporain","Pierre de Volvic","50","4","2010","2012","Ceci est la description du centre Jaude 2");
-            WriteLine("ToString du Batiment avec détail maximal:\n"+batiment3.ToString());
+            
+            WriteLine("ToString du Batiment avec détail maximal:\n"+Stub.CreerBatimentComplet().ToString());
 
             WriteLine("------------------------------------------------------------------------------------------------------------\n");
             WriteLine("\n2) Instanciation de la classe Ville:\n\n");
 
-            Ville ville1 = new Ville("Clermont-Ferrand");
-            WriteLine("ToString de la ville juste après création:\n" + ville1.ToString());
+            Ville villetest = Stub.CreerVille();
+
+            WriteLine("ToString de la ville juste après création:\n" + villetest.ToString());
             WriteLine("-    -   -   -   -   -");
-            bool ajoutsimple = ville1.AjouterBatimentSimple("Cathédrale", "France", "Clermont - Ferrand", "Ceci est la decription de la cathédrale de Clermont - Ferrand");
+
+            bool ajoutsimple = villetest.AjouterBatimentSimple("Cathédrale", "France", "Clermont - Ferrand", "Ceci est la decription de la cathédrale de Clermont - Ferrand");
             WriteLine($"Test variable ajoutsimple:{ajoutsimple}");
-            WriteLine("ToString de la ville juste après ajout simple d'un batiment:\n" +ville1.ToString());
+            WriteLine("ToString de la ville juste après ajout simple d'un batiment:\n" +villetest.ToString());
             WriteLine("-    -   -   -   -   -");
-            bool ajoutcomplet = ville1.AjouterBatimentComplet("Centre Jaude 2", "France", "Clermont-Ferrand", "Centre de Clermont", "Place de Jaude", "M.Jenesaispas", "M.Jenesaisqui",
+
+            bool ajoutcomplet = villetest.AjouterBatimentComplet("Centre Jaude 2", "France", "Clermont-Ferrand", "Centre de Clermont", "Place de Jaude", "M.Jenesaispas", "M.Jenesaisqui",
                 "Comtemporain", "Pierre de Volvic", "50", "4", "2010", "2012", "Ceci est la description du centre Jaude 2");
             WriteLine($"Test variable ajoutcomplet:{ajoutcomplet}");
-            WriteLine("ToString de la ville juste après ajout complet d'un batiment:\n" + ville1.ToString());
+            WriteLine("ToString de la ville juste après ajout complet d'un batiment:\n" + villetest.ToString());
             WriteLine("-    -   -   -   -   -");
-            bool supprimerbatiment = ville1.SupprimerBatiment("Cathédrale");
+
+            bool supprimerbatiment = villetest.SupprimerBatiment("Cathédrale");
             WriteLine($"Test variable suppression:{supprimerbatiment}");
-            WriteLine("ToString de la ville juste après suppression d'un batiment:\n" + ville1.ToString());
+            WriteLine("ToString de la ville juste après suppression d'un batiment:\n" + villetest.ToString());
             WriteLine("-    -   -   -   -   -");
-            bool modifierbatiment = ville1.ModifierBatiment("Centre Jaude 2", "CARRE JAUDE 2", "France", "Clermont-Ferrand", "Centre de Clermont", "Place de Jaude", "M.Jenesaispas", "M.Jenesaisqui",
+            bool modifierbatiment = villetest.ModifierBatiment("Centre Jaude 2", "CARRE JAUDE 2", "France", "Clermont-Ferrand", "Centre de Clermont", "Place de Jaude", "M.Jenesaispas", "M.Jenesaisqui",
                 "Comtemporain", "Pierre de Volvic", "50", "4", "2010", "2012", "Ceci est la description du carre Jaude 2");
             WriteLine($"Test de la variable modification:{modifierbatiment}");
-            WriteLine("ToString de la ville après modification d'un batiment:\n" + ville1.ToString());
+            WriteLine("ToString de la ville après modification d'un batiment:\n" + villetest.ToString());
 
             WriteLine("------------------------------------------------------------------------------------------------------------\n");
             WriteLine("\n3) Instanciation de la classe Manager:\n\n");
@@ -65,21 +70,21 @@ namespace Test
             WriteLine("-    -   -   -   -   -");
             bool ajoutsimplemanager = manager.AjouterBatimentSimple("Clermont-Fd","Cathédrale", "France", "Clermont - Ferrand", "Ceci est la decription de la cathédrale de Clermont - Ferrand");
             WriteLine($"Test variable ajoutsimplemanager:{ajoutsimplemanager}");
-            WriteLine("ToString de Manager juste après ajout simple d'un batiment:\n" + ville1.ToString());
+            WriteLine("ToString de Manager juste après ajout simple d'un batiment:\n" + villetest.ToString());
             WriteLine("-    -   -   -   -   -");
             bool ajoutcompletmanager = manager.AjouterBatimentComplet("Clermont-Fd","Centre Jaude 2", "France", "Clermont-Ferrand", "Centre de Clermont", "Place de Jaude", "M.Jenesaispas", "M.Jenesaisqui",
                 "Comtemporain", "Pierre de Volvic", "50", "4", "2010", "2012", "Ceci est la description du centre Jaude 2");
             WriteLine($"Test variable ajoutcompletmanager:{ajoutcompletmanager}");
-            WriteLine("ToString de Manager juste après ajout complet d'un batiment:\n" + ville1.ToString());
+            WriteLine("ToString de Manager juste après ajout complet d'un batiment:\n" + villetest.ToString());
             WriteLine("-    -   -   -   -   -");
             bool supprimerbatimentmanager = manager.SupprimerBatiment("Clermont-Fd","Cathédrale");
             WriteLine($"Test variable suppressionbatimentmanager:{supprimerbatimentmanager}");
-            WriteLine("ToString de Manager juste après suppression d'un batiment:\n" + ville1.ToString());
+            WriteLine("ToString de Manager juste après suppression d'un batiment:\n" + villetest.ToString());
             WriteLine("-    -   -   -   -   -");
             bool modifierbatimentmanager = manager.ModifierBatiment("Clermont-Fd","Centre Jaude 2", "CARRE JAUDE 2", "France", "Clermont-Ferrand", "Centre de Clermont", "Place de Jaude", "M.Jenesaispas", "M.Jenesaisqui",
                 "Comtemporain", "Pierre de Volvic", "50", "4", "2010", "2012", "Ceci est la description du carre Jaude 2");
             WriteLine($"Test de la variable modificationbatimentmanager:{modifierbatimentmanager}");
-            WriteLine("ToString de Manager après modification d'un batiment:\n" + ville1.ToString());
+            WriteLine("ToString de Manager après modification d'un batiment:\n" + villetest.ToString());
             bool supprimerville = manager.SupprimerVille("Clermont-Fd");
             WriteLine($"Test de la variable de suppression d'une ville:{supprimerville}");
             WriteLine("ToString de Manager juste après la suppression d'une ville:\n" + manager.ToString());
