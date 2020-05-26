b1976afb2266a78be291c7b923cf34034212123e
lorusak <lorusak@3994cbb5-1302-4bbd-8812-0d1d036f0faf>
2020-05-26 15:04:01 +0000

    Ajout fichier Word et de la bibliothèque de classes Data

---
+++ trunk/source/ReasArch/Data/Stub.cs
@@ -0,0 +1,30 @@
+﻿using Modele;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Data
+{
+    public class Stub
+    {
+        public static Ville CreerVille()
+        {
+            Ville ville1 = new Ville("Clermont");
+            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
+            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
+            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
+            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
+            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
+            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
+            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
+            ville1.AjouterBatimentSimple("Centre jaude", "France", "Clermont-Ferrand", "Description du batiment");
+            return ville1;
+        }
+
+
+
+    }
+}
