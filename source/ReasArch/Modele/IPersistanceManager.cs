1617897fb8c3653ee0f0970aeff903b9f9ffd046
mamanin <mamanin@3994cbb5-1302-4bbd-8812-0d1d036f0faf>
2020-06-15 21:43:36 +0000

    oubli d'un fichier

---
+++ source/ReasArch/Modele/IPersistanceManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modele
+{
+    public interface IPersistanceManager
+    {
+        /// <summary>
+        /// Fonction sesant le chargement des fichier
+        /// </summary>
+        Monde ChargeDonnées();
+
+        /// <summary>
+        /// Fonction fesant la sauvegarde
+        /// </summary>
+        void SauvegardeDonnées(Monde LeMonde);
+    }
+}
